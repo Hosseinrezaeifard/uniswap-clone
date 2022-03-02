4dc6725647f39a5b3cf6439ff186aa302a9121ec
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2022-03-02 05:48:54 -0500

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     sanityToken:
-      "sk5v3d2LqYL4muG5JBPmpd9ckdYAA8CMut9K9GU5MpL98Uv8jiUSUUIL8bKRuZ5wyKJnpeRUpNAM0BW6ASrychFPmDIn53bRsrxZMT3CTBXMdUVSmZjn6iEIsWSt5l1kOtRlGIGFxExBskxnjM50N7g771hNz4WDIfIawUO58JFUOv7X1kQW",
-    sanityProjectId: "w0c8siqj",
+      "Your Sanity Token",
+    sanityProjectId: "Your Sanity Project Id",
   },
 };
 
