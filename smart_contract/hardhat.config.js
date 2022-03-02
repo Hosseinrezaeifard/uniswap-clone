d4264135263acb1fcdeb11f9863dd79f52bd313f
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2022-03-02 05:48:02 -0500

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,9 +4,9 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/yVlMdOGaTiqvkXXeK3FUlvdhkZMup1vy",
+      url: "Your HTTP Api key from your node provider (_example: Alchemy)"
       accounts: [
-        "205b0270263fe97f08e6eb1f8b520324db151de444d65f5318f946b4ac39bf5b",
+        "Your Metamask private key",
       ],
     },
   },
